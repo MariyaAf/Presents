ae537eec0bd22731e2e9615d9075c090e66415e6
Мария <maaf23@yandex.ru>
2017-12-26 19:55:32 +0500

    Class "Product"

---
+++ Presents/Presents/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presents
+{
+    public class Product
+    {
+        public double Price { get; set; }
+        public string Name { get; set; }
+        public virtual double GetDiscountPrice(User user)
+        {
+            if (user.Spent < 300)
+            {
+                return Price;
+            }
+
+            if (user.Spent < 1000)
+            {
+                return Price * 0.9;
+            }
+            return Price * 0.8;
+        }
+    }
+}
