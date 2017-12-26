6b7ee936e1906a4ec5c7977062efefdbbe969170
Мария <maaf23@yandex.ru>
2017-12-26 19:48:19 +0500

    class "Informer"

---
+++ Presents/Presents/Informer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    public class Informer
+    {
+        public void Buy(User user, Product product)
+        {
+            double price = product.GetDiscountPrice(user);
+            user.ReduceBalance(price);
+            Console.WriteLine($"Здравствуйте,{user.Name}. Вы купили {product.Name} за {price}.");
+            Console.WriteLine($"Ваш баланс {user.Balance}");
+        }
+    }
+}
